1885ec9675b6bb0f81b1727d709771f0bba6e58a
Jeremías <jeremias4810@hotmail.com>
2024-08-08 19:50:41 -0300

    Add CategoriesCard

--- app/src/main/java/com/example/reloj/MainActivity.kt
+++ app/src/main/java/com/example/reloj/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyListScope
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Card
@@ -27,10 +28,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.reloj.ui.theme.RelojTheme
 
 
@@ -44,8 +49,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier
                         .fillMaxSize()
                         .background(Color.Red),
-                        color = MaterialTheme.colorScheme.background
-                ){
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     ViewContainer()
 
                 }
@@ -54,41 +59,47 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Preview
 @Composable
 fun ViewContainer() {
     Scaffold(
         content = {
-            MiUI()}
+            MiUI()
+        }
     )
 }
 
 @Composable
 fun LazyRowContent() {
     Log.i("Corcho", "Debería entrar en LazyRowContent")
-    LazyRow(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth() // Asegura que LazyRow ocupe todo el ancho disponible
+    LazyRow(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth() // Asegura que LazyRow ocupe todo el ancho disponible
     ) {
         // Usar item en lugar de items si solo hay un conjunto fijo de elementos
         item {
             Row { // Envolver en Row para alinear las tarjetas horizontalmente
-                Card(modifier = Modifier
-                    .size(70.dp)
-                    .padding(end = 8.dp) // Añadir espaciado entre tarjetas
+                Card(
+                    modifier = Modifier
+                        .size(70.dp)
+                        .padding(end = 8.dp) // Añadir espaciado entre tarjetas
                 ) {
                     Text(text = "Todos", modifier = Modifier.padding(8.dp))
                 }
-                Card(modifier = Modifier
-                    .size(70.dp)
-                    .padding(end = 8.dp)
+                Card(
+                    modifier = Modifier
+                        .size(70.dp)
+                        .padding(end = 8.dp)
                 ) {
                     Text(text = "Mañana", modifier = Modifier.padding(8.dp))
                 }
-                Card(modifier = Modifier
-                    .size(70.dp)
-                    .padding(end = 8.dp)
+                Card(
+                    modifier = Modifier
+                        .size(70.dp)
+                        .padding(end = 8.dp)
                 ) {
                     Text(text = "Nuevo", modifier = Modifier.padding(8.dp))
                 }
@@ -96,6 +107,44 @@ fun LazyRowContent() {
         }
     }
 }
+
+@Composable
+fun CategoriesCard(title: String, text: String, value: Boolean) {
+    Card(
+        modifier = Modifier
+            .height(80.dp)
+            .width(120.dp),
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+
+            ) {
+            Column {
+
+                Box(modifier = Modifier.weight(1f),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = title, modifier = Modifier.fillMaxWidth(), fontSize = 20.sp,textAlign = TextAlign.Center )
+                }
+                Box(modifier = Modifier.weight(2f),contentAlignment = Alignment.Center) {
+                    Row (modifier = Modifier.fillMaxSize().align(Alignment.Center),
+                        ){
+
+                        Text(text = text, modifier = Modifier.align(Alignment.Bottom))
+                        val estado = value
+                        val checkedState = remember { mutableStateOf(estado) }
+                        val checked = checkedState.value
+                        Switch(
+                            checked = checkedState.value,
+                            onCheckedChange = { checkedState.value = it })
+                    }
+                }
+            }
+        }
+    }
+
+}
+
 
 @Composable
 fun MiCarta(texto: String, value: Boolean, modifier: Modifier = Modifier) {
@@ -107,25 +156,32 @@ fun MiCarta(texto: String, value: Boolean, modifier: Modifier = Modifier) {
         val checkedState = remember { mutableStateOf(estado) }
         val checked = checkedState.value
         Log.i("Corcho", "lo que aparece en checked es${checked}")
-        Switch(checked = checkedState.value, onCheckedChange = {checkedState.value = it} )
-        if (checked){
+        Switch(checked = checkedState.value, onCheckedChange = { checkedState.value = it })
+        if (checked) {
             Log.i("Corcho", "el valor es verdadero, ${checked}")
             LazyRowContent()
-        }else{Log.i("Corcho", "el valor es falso ${checked}")}
+        } else {
+            Log.i("Corcho", "el valor es falso ${checked}")
+        }
     }
 }
+
+
 @Composable
 fun MiUI() {
     Column {
 
         Box(
             modifier = Modifier
-                .height(50.dp)
                 .padding(16.dp)
+                .fillMaxWidth()
         ) {
             Row {
-                Text(text = "Hola")
-                Text(text = "Chau")
+                CategoriesCard(title = "Todos", text = "Todos", value = false)
+                Spacer(modifier = Modifier.width(4.dp))
+                CategoriesCard(title = "Mañana", text = "Desactivado", value = false)
+                Spacer(modifier = Modifier.width(4.dp))
+                CategoriesCard(title = "Tarde", text = "Desactivado", value = false)
             }
 
         }
