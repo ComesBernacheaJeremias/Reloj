8f001b6ce37f542e75c7132ff970a0a8ff796d53
Jeremías <jeremias4810@hotmail.com>
2024-08-09 21:12:19 -0300

    Add

--- app/src/main/java/com/example/reloj/ui/theme/CategoriesCard.kt
+++ app/src/main/java/com/example/reloj/ui/theme/CategoriesCard.kt
@@ -1,27 +1,37 @@
 package com.example.reloj.ui.theme
 
 
+import android.util.Log
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 
 
 @Composable
@@ -62,12 +72,13 @@ fun CategoriesCard(title: String, text: String, value: Boolean) {
     }
 
 }
+
 @Composable
-fun AddCategories(){
+fun AddCategories() {
 
     val openDialog = remember { mutableStateOf(false) }
 
-    Card( modifier = Modifier
+    Card(modifier = Modifier
         .height(80.dp)
         .width(150.dp)
         .clickable { openDialog.value = true }
@@ -76,7 +87,7 @@ fun AddCategories(){
         Box(modifier = Modifier.fillMaxSize(), Alignment.Center) {
             Text(
                 text = "Agregar",
-               // modifier = Modifier.fillMaxSize(),
+                // modifier = Modifier.fillMaxSize(),
                 textAlign = TextAlign.Center
             )
         }
@@ -86,52 +97,59 @@ fun AddCategories(){
         alertDialogDoc(openDialog)
     }
 }
+
 @Composable
 fun alertDialogDoc(openDialog: MutableState<Boolean>) {
     val openDialog = remember { mutableStateOf(true) }
+    var text by rememberSaveable { mutableStateOf("") }
 
     if (openDialog.value) {
-        AlertDialog(
+        Dialog(
             onDismissRequest = {
                 openDialog.value = false
-            },
-            title = {
-                Text(text = "Documentación  de AlertDialog")
-            },
-            text = {
-                Text(
-                    "Descripción de la alerta de ejemplo de material 2."
-                )
-            },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
-                    }
+            }) {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(375.dp)
+                    .padding(16.dp),
+                shape = RoundedCornerShape(16.dp),
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
                 ) {
-                    Text("Aceptar")
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = {
-                        openDialog.value = false
+                    TextField(
+                        value = text,
+                        onValueChange = { text = it },
+                        placeholder = { Text("Nombre") },
+                        label = { Text(text = "Nueva Categoria") },
+                        singleLine = true
+                    )
+                    Button(onClick = { AdderNewCategories(text = text) }) {
+                        Text("Add Category")
+                        
                     }
-                ) {
-                    Text("Salir")
                 }
+
             }
-        )
+
+        }
     }
 }
 
 
 @Composable
-fun AdderNewCategories() {
+fun AdderNewCategories(text: String) {
+    val texto = text
+    Log.i("Corcho", "lo que aparece en addernewcategories es ${texto}")
 
 }
+
 @Composable
-fun AllCategories(){
+fun AllCategories() {
 
 }
 
@@ -139,5 +157,4 @@ fun AllCategories(){
 @Composable
 fun PreviewCard() {
 
-    AddCategories()
 }
