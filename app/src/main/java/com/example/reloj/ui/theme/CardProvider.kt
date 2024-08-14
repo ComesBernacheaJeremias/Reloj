f3b112026b7df09647b06da5e5de73927af994b2
Jeremías <jeremias4810@hotmail.com>
2024-08-14 11:59:34 -0300

    Adder algunos dialogos

--- app/src/main/java/com/example/reloj/ui/theme/CardProvider.kt
+++ app/src/main/java/com/example/reloj/ui/theme/CardProvider.kt
@@ -3,12 +3,16 @@ package com.example.reloj.ui.theme
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 
+//las clases son tipos nuevos de datos (objetos)
+
 class CartasViewModel : ViewModel() {
     // Usamos `mutableStateListOf` para crear una lista observable por Compose.
+
+    //Estas son las propiedades (atributos) que definen al objeto:
     var cartas = mutableStateListOf("Carta 1", "Carta 2", "Carta 3")
         private set
 
-    fun agregarCarta(nuevaCarta: String) {
+    fun agregarCarta(nuevaCarta: String) {  //<--estos son los metodos. las acciones que puede realizar.
         cartas.add(nuevaCarta)
     }
 }
